c863fbd979b4b9581318d593cfd1fce7ef9c1e0b
ahamm-prequal2021 <a.hamm@codeur.online>
2021-10-26 20:34:27 +0200

    credentials

--- actions/database.php
+++ actions/database.php
@@ -3,7 +3,7 @@ if(!isset($_SESSION)) {
     session_start();
 }
 try{
-    if(strpos($_SERVER['REQUEST_URI'], 'codeur.online') !== false){
+    if(strpos($_SERVER['REQUEST_URI'], 'codeur') !== false){
         $db = new PDO('mysql:host=localhost;dbname=alexandreh_forum;charset=utf8;', 'alexandreh', 'qCPwobB/1HD+vw==');
     }else{
         $db = new PDO('mysql:host=localhost;dbname=forum;charset=utf8;', 'root', '');
