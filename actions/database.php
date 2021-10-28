4f0f4842d00540af1cccc1cb76490eb41ee33815
ahamm-prequal2021 <a.hamm@codeur.online>
2021-10-28 11:27:54 +0200

    fixed post methods

--- actions/database.php
+++ actions/database.php
@@ -3,11 +3,11 @@ if(!isset($_SESSION)) {
     session_start();
 }
 try{
-    // if(preg_match ('#^/adgloriam-forum/#', $_SERVER['REQUEST_URI'])){
-    //     $db = new PDO('mysql:host=localhost;dbname=alexandreh_forum;charset=utf8;', 'alexandreh', 'qCPwobB/1HD+vw==');
-    // }else{
+    if(preg_match ('#^/adgloriam-forum/#', $_SERVER['REQUEST_URI'])){
+        $db = new PDO('mysql:host=localhost;dbname=alexandreh_forum;charset=utf8;', 'alexandreh', 'qCPwobB/1HD+vw==');
+    }else{
         $db = new PDO('mysql:host=localhost;dbname=forum;charset=utf8;', 'root', '');
-    // }
+    }
 }catch(Exception $e){
     die('Error ' . $e->getMessage());
 }
